a7605b39fe12d3adf14fef79d67481fa2ad704b3
Anatoly Bubenkov <bubenkoff@gmail.com>
2012-04-12 15:24:43 +0300

    fix setup script, add ignores

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='zojax.gae.jsonrpc',
     version='0.1.0',
     author="Yaroslav D.",
-    author_email='blw0rm@gmail.com',
+    author_email='developers@zojax.com',
     description=("Implements JSON/RPC for Google App Engine (Python)."),
     long_description=(
         read('README.rst')
@@ -32,6 +32,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
         ],
     url='',
+    packages=['zojax', 'zojax.gae', 'zojax.gae.jsonrpc'],
     package_dir = {'': 'src'},
     include_package_data=True,
     namespace_packages=['zojax', 'zojax.gae'],
